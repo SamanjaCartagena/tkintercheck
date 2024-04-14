cefe1e7dfb63ecd04557ea195e2ff72dfe730bae
Samanja Cartagena <c.samanja09@Pabs.local>
2024-04-14 14:06:50 -0400

    first commit

--- toggle/check.py
+++ toggle/check.py
@@ -6,28 +6,53 @@ Created on Sat Apr 13 23:43:42 2024
 """
 
 from tkinter import *
-import ttkbootstrap as tb
 
-root=tb.Window(themename='superhero')
+root=Tk()
+root.title('Simple Calculator')
+e=Entry(root,width=35, borderwidth=5)
+e.grid(row=0,column=0, columnspan=3, padx=10,pady=10)
 
-root.title("TTK Bootstrap!")
-root.geometry('500x350')
 
-def checker():
-    if var1.get() == 1:
-        my_label.config(text="Checked!")
-    else:
-        my_label.config(text="Unchecked!")
-my_label=Label(text="Click the checkbutton below", font=("Helvetica",18))
-my_label.pack(pady=(40,10))
 
-var1=IntVar()
-my_check=tb.Checkbutton(bootstyle="primary", 
-                        text="Check",
-                        variable=var1,
-                        onvalue=1,
-                        offvalue=0,
-                        command=checker)
-my_check.pack(pady=10)
+def button_click(number):
+    e.insert(0, number)
+#Define Buttons
+
+button_1=Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2=Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3=Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4=Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5=Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6=Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7=Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8=Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9=Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0=Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+
+button_add=Button(root,text="+", padx=39, pady=20, command=button_click)
+button_equal=Button(root,text="=", padx=91, pady=20, command=button_click)
+button_clear=Button(root,text="Clear", padx=79, pady=20,command=button_click)
+
+
+#Put the buttons on the screen
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=1)
+button_equal.grid(row=5,column=1, columnspan=2)
+
+myButton=Button(root, text="Enter your Stock")
+
+
+
+
 
 root.mainloop()
